8ed788ba85af87054ce82042d8c9a695dd97f748
ajay0398 <ajay.mahato0398@gmail.com>
2023-04-07 21:34:51 +0530

    initial commit

---
+++ src/main/java/com/hub/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.hub.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, I am Alive 😝 Welcome to Hub 👉";
+    }
+}
---
+++ src/test/java/com/hub/HubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
